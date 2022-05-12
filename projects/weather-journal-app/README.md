67908653d900567d69887c81d266da16872746b7
xZezox <ozozzezo4@gmail.com>
2022-05-12 19:03:09 +0200

    Completed weather journal app project

--- projects/weather-journal-app/README.md
+++ projects/weather-journal-app/README.md
@@ -1,10 +1,25 @@
 # Weather-Journal App Project
 
-## Overview
-This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
 
-## Instructions
-This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
 
-## Extras
-If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+# Demo-Preview
+
+<!-- Add a demo for your project -->
+
+<!-- After you have written about your project, it is a good idea to have a demo/preview(**video/gif/screenshots** are good options) of your project so that people can know what to expect in your project. You could also add the demo in the previous section with the product description.
+Here is a random GIF as a placeholder.
+ -->
+
+![Imgur Image](https://i.imgur.com/4oiY1qU.png)
+
+# Project description
+This is an asynchronous web app that uses Web API and user data to dynamically update the UI with current date and weather for the specified US zipcode.
+
+# Usage
+open command line in the following directory projects/weather-journal-app/
+Run the server using the following command: "node ./server.js"
+
+<!-- This is optional and it is used to give the user info on how to use the project after installation. This could be added in the Installation section also. -->
+
+# Credits
+Zeyad Alaa Eldin Mohamed
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -2,4 +2,62 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?units=imperial'
+let apiKey = '&appid=e0b77c3ea2f0d5cfa389696b6e84d07d';
+
+// Async GET
+const getData = async (url) => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const data = await request.json()
+        if (data.cod !== 200) alert("please enter a valid US zipcode")
+        else return (data)
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+};
+const postData = async (url = '', data = {}) => {
+
+    const response = await fetch(url, {
+        method: 'POST', credentials: 'same-origin', headers: {
+            'Content-Type': 'application/json',
+        }, body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+
+    try {
+        return await response.json();
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+
+async function getWeather(e) {
+    let zipCode = "&zip=" + document.getElementById('zip').value;
+    let content = document.getElementById("feelings").value;
+
+    getData(baseURL + zipCode + apiKey).then(newData => {
+        postData("/addData", {date: newDate, temp: newData.main.temp, content});
+    }).then(updateUI)
+}
+
+document.getElementById("generate").addEventListener("click", getWeather)
+
+
+async function updateUI() {
+    const request = await fetch('/all');
+    try {
+        const allData = await request.json();
+        document.getElementById("entry").classList.remove("invisible");
+        document.getElementById('date').innerText = allData.date;
+        document.getElementById('temp').innerHTML = Math.round(allData.temp) + '°F';
+        document.getElementById('content').innerHTML = allData.content;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
